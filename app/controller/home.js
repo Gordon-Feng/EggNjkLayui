111d302ebb6d3338eccbaf24f20d69a7ae28c1fc
冯汝瀛 <1344135036@qq.com>
2019-07-02 18:10:13 +0800

    修改名称

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,7 +3,7 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
+  async Index() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
