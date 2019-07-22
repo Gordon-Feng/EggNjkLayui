728ff2da71860257c79fe1c7318c6ff07a91e753
冯汝瀛 <1344135036@qq.com>
2019-07-22 15:15:27 +0800

    检查登录状态

--- app/controller/index.js
+++ app/controller/index.js
@@ -3,13 +3,21 @@
 const Controller = require('egg').Controller;
 
 class IndexController extends Controller {
+
+  /**
+   * 首页(用于嵌套iframe)
+   */
   async Index() {
     const { ctx } = this;
-    await ctx.render('Index.njk' , { 
-      title: '首页' , 
-      img_host: process.env.IMG_HOST ,
-      account_info: { club_name: ctx.session.club_name, club_account: ctx.session.club_account, club_logo: ctx.session.club_logo } 
-    }); // title用于显示在浏览器标签中
+    if(!ctx.session.club_account){
+      await ctx.redirect('/login');
+    }else{
+      await ctx.render('Index.njk' , { 
+        title: '首页' , 
+        img_host: process.env.IMG_HOST ,
+        account_info: { club_name: ctx.session.club_name, club_account: ctx.session.club_account, club_logo: ctx.session.club_logo } 
+      }); // title用于显示在浏览器标签中
+    }
   }
 }
 
